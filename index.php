69dfed094c054c7bc6f67068a9ac9676d7a80a4a
Nikko Zabala <nikko.zabala@gmail.com>
2020-04-14 23:32:50 +0800

    scorecard index

---
+++ index.php
@@ -0,0 +1,21 @@
+<!--Added Nikko Zabala visit me at www.nikkozabala.me -->
+
+<?php
+
+$loc = '/scorecard/public';
+header("Location: $loc");
+die(0);
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Personiv Portal</title>
+</head>
+<body>
+<h1>Hello World</h1>
+
+<a href="/scorecard/public">DCO Scorecard</a>
+</body>
+</html>
